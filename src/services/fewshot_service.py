5bdd14ff70cad0ae2ae50c5a6b5f432ed4a316e9
Ritesh-ch7 <riteshch098@gmail.com>
2024-02-26 15:42:17 +0530

    Did minor exception handling

--- src/services/fewshot_service.py
+++ src/services/fewshot_service.py
@@ -16,42 +16,47 @@ llm1 = ChatOpenAI(openai_api_key=api_key, temperature=0.3)
 def few_shot_body_template(ticket_id, requester_name, priority, severity, text, user_examples, trace_id : str = None):
     if (trace_id == None):
         trace_id = str(uuid.uuid4())
+
+    try:
+
     
-    examples = user_examples
+        examples = user_examples
 
-    example_template="""
-    User: Write an acknowledgement email body for ticket with ticket id- {ticket_id} to the requester- {requester_name} stating that you are working regarding the issue- {text}. The ticket's priority is {priority} and also the severity of the ticket is {severity} where 4 means high severity and 1 has low severity. If the requester's priority and severity is low, don't mention the priority and severity explicitly in the mail but if the priority and severity is high then stress in the mail that we are working really hard and its our topmost priority like that. Generate the mail body having content of around 5-6 lines. 
-    AI: {ref}
-    """
-
-
-    example_prompt=PromptTemplate(
-    input_variables=["ticket_id","requester_name","text","priority","severity","ref"],
-    template=example_template
-    )
-
-    prefix=""" 
-    You have to write a acknowledgement mail body, by taking the given example as reference. If the description of ticket provided in suffix cannot be answered or if it is out of context like "tell me a joke", "how are you", etc., then provide the answer with 'I don't know, kindly give me a detailed ticket description' ."""
+        example_template="""
+        User: Write an acknowledgement email body for ticket with ticket id- {ticket_id} to the requester- {requester_name} stating that you are working regarding the issue- {text}. The ticket's priority is {priority} and also the severity of the ticket is {severity} where 4 means high severity and 1 has low severity. If the requester's priority and severity is low, don't mention the priority and severity explicitly in the mail but if the priority and severity is high then stress in the mail that we are working really hard and its our topmost priority like that. Generate the mail body having content of around 5-6 lines. 
+        AI: {ref}
+        """
 
 
-    suffix=""" 
-    User: Now generate a mail body which is almost similar to the above given examples and make sure the new mail generated is matching the current ticket details which is being provided now where the ticket id is-{ticket_id}, the requester name is -{requester_name}, the ticket description is- {text}. The ticket's priority is - {priority} and also the severity of the ticket is -{severity} where 4 means high severity and 1 has low severity.
-    AI: """
+        example_prompt=PromptTemplate(
+        input_variables=["ticket_id","requester_name","text","priority","severity","ref"],
+        template=example_template
+        )
+
+        prefix=""" 
+        You have to write a acknowledgement mail body, by taking the given example as reference. If the description of ticket provided in suffix cannot be answered or if it is out of context like "tell me a joke", "how are you", etc., then provide the answer with 'I don't know, kindly give me a detailed ticket description' ."""
 
 
-    few_shot_template=FewShotPromptTemplate(
-    examples=examples,
-    example_prompt=example_prompt,
-    prefix=prefix,
-    suffix=suffix,
-    input_variables=["ticket_id","requester_name","text","priority","severity"]
-    )
+        suffix=""" 
+        User: Now generate a mail body which is almost similar to the above given examples and make sure the new mail generated is matching the current ticket details which is being provided now where the ticket id is-{ticket_id}, the requester name is -{requester_name}, the ticket description is- {text}. The ticket's priority is - {priority} and also the severity of the ticket is -{severity} where 4 means high severity and 1 has low severity.
+        AI: """
 
 
-    query=few_shot_template.format(ticket_id=ticket_id, requester_name=requester_name, text=text, priority=priority, severity=severity, example=user_examples)
+        few_shot_template=FewShotPromptTemplate(
+        examples=examples,
+        example_prompt=example_prompt,
+        prefix=prefix,
+        suffix=suffix,
+        input_variables=["ticket_id","requester_name","text","priority","severity"]
+        )
 
 
-    return llm1.invoke(query).content
+        query=few_shot_template.format(ticket_id=ticket_id, requester_name=requester_name, text=text, priority=priority, severity=severity, example=user_examples)
+
+
+        return llm1.invoke(query).content
+    except Exception as e:
+        return f"error in test : {e}"
 
 
 
