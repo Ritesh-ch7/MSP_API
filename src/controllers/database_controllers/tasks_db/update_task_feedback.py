95cacf2d2b206b3372383026a2d1cfab415d1748
yashwanth <yashwanthpapishetty7@gmail.com>
2024-02-29 16:21:23 +0530

    added feedback func

--- src/controllers/database_controllers/tasks_db/update_task_feedback.py
+++ src/controllers/database_controllers/tasks_db/update_task_feedback.py
@@ -11,16 +11,25 @@ from datetime import datetime
 from src.utils.constants import *
 from sqlalchemy import func
 
+<<<<<<< HEAD
 async def update_task_feedback(llm_id, db, trace_id):
+=======
+def update_task_feedback(llm_id, db, task_feedback trace_id):
+>>>>>>> parent of a2b204f (added feedback update function)
     if(not trace_id):
         trace_id = str(uuid.uuid4())
 
     try:
+<<<<<<< HEAD
         task = db.query(Task).filter(Task.LlmId == llm_id).order_by(Task.Id.desc()).first()
         if task:
             task.Feedback = 'Negative'
             task.UpdatedAt = func.now()
             db.commit()
+=======
+       db.query(Task).filter(Task.LlmId == llm_id).order_by(Task.Id.desc()).update({Task.Feedback : 'Negative', Task.UpdatedAt : func.now()})
+       db.commit()
+>>>>>>> parent of a2b204f (added feedback update function)
 
     except Exception as e:
         logger.error(f'{trace_id} Could not update the feedback of the task with id {llm_id}')
